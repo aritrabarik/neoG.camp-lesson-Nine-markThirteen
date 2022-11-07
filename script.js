f1378fb23606c81ec9da069d9a8c5f8ee679c247
Aritra Barik <61828095+aritrabarik@users.noreply.github.com>
2022-11-08 00:07:14 +0530

    completed ex-06 & ex-08

--- script.js
+++ script.js
@@ -103,9 +103,78 @@ function getNextDate(date) {
     var daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
     if (month === 2) {
-        // check for leap year
+        if (isLeapYear(year)) {
+            if (day > 29) {
+                day = 1;
+                month++;
+            }
+        } else {
+            if (day > 28) {
+                day = 1;
+                month++;
+            }
+        }
     } else {
+        if (day > daysInMonth[month - 1]) {
+            day = 1;
+            month++;
+        }
+    }
+
+    if (month > 12) {
+        month = 1;
+        year++;
+    }
+
+    return {
+        day: day,
+        month: month,
+        year: year,
+    };
+}
+
+// ex08: get date from user and console.log it on button click
+
+function getNextPalindromeDate(date) {
+    var count = 0;
+    var nextDate = getNextDate(date);
+
+    while (1) {
+        count++;
+        var isPalindrome = checkPalindromeForAllDateFormats(nextDate);
+        if (isPalindrome) break;
+
+        nextDate = getNextDate(nextDate);
+    }
+
+    return [count, nextDate];
+}
+
+var dateInput = document.querySelector("#bday-input");
+var showBtn = document.querySelector("#show-btn");
+var outputEl = document.querySelector("#output");
+
+function clickHandler() {
+    var bdayStr = dateInput.value;
+
+    if (bdayStr !== "") {
+        var listOfDate = bdayStr.split("-");
+
+        var date = {
+            day: Number(listOfDate[2]),
+            month: Number(listOfDate[1]),
+            year: Number(listOfDate[0]),
+        };
+
+        var palindromeDate = checkPalindromeForAllDateFormats(date);
+
+        if (palindromeDate)
+            outputEl.innerText = `Yay! Your birthday is a palindrome! 🥳🥳`;
+        else {
+            var [count, nextDate] = getNextPalindromeDate(date);
+            outputEl.innerText = `The next palindrome date is ${nextDate.day}-${nextDate.month}-${nextDate.year}, you missed it by ${count} days 😢😢`;
+        }
     }
 }
 
-function getNextPalindromeDate(date) {}
+showBtn.addEventListener("click", clickHandler);
